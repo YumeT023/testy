75f022b42ccf2aa499c3fab96a298d9515cbeddf
Yume <hei.hajatiana@gmail.com>
2023-12-24 21:25:16 +0300

    fixup: re-structure directories

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export * from "./impl";
-export * from "./decorator";
-export * from "./platform";
-
-// types
-export * from "./types";
+export * from "@testy/core";
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Ctor<T> extends Function {
-  new (...args: unknown[]): T;
-}
-
-export type ClassDecorator = (type: Ctor<unknown>, ctx: ClassDecoratorContext) => any;
-export type ClassMethodDecorator = (f: Function, ctx: ClassMethodDecoratorContext) => any;
-
-export type * from "./platform/runnable_test";
-export type * from "./impl/types";
